001746d750108bfebdd423d961ea7ae9faa3dada
Kevin Chan <kevinchan4042@gmail.com>
2021-07-12 09:21:27 -0700

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/kc/helloworld/Hello.java
@@ -0,0 +1,14 @@
+package com.kc.helloworld;
+
+/**
+ *
+ * @author Kevin Chan
+ * email: kevinchan4042@gmail.com
+ * date: 7/12/2021
+ * purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
