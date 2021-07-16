12ea9f845c734fddc43a70be0fc932443d6d21b4
Kevin Chan <kevinchan4042@gmail.com>
2021-07-16 16:36:53 -0700

    SimpleCalculator class

---
+++ Exercises/src/main/java/com/kc/exercises/classes/SimpleCalculator.java
@@ -0,0 +1,17 @@
+
+package com.kc.exercises.classes;
+
+public class SimpleCalculator {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public double div(double a, double b){
+        return a/b;
+    }
+}
